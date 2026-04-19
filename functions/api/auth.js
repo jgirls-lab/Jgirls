8d8c67d22f588fc83003f6585c6d8119a95cee4a
jgirls-lab <pointwanko3232@gmail.com>
2026-04-20 01:05:25 +0900

    Update auth.js

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -1,13 +1,5 @@
 export async function onRequest(context) {
-  const clientId = context.env.GITHUB_CLIENT_ID;
-
-  // GitHub に返ってくる URL を自動で /api/callback に変換
-  const redirectUri = `${context.request.url.replace('/api/auth', '/api/callback')}`;
-
-  const url = new URL("https://github.com/login/oauth/authorize");
-  url.searchParams.set("client_id", clientId);
-  url.searchParams.set("redirect_uri", redirectUri);
-  url.searchParams.set("scope", "repo,user");
-
-  return Response.redirect(url.toString(), 302);
+  return new Response(JSON.stringify(context.env), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
